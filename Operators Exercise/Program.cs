999f4c7d397cf8e632723e2d68d4a1ed48e4e0c0
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-11-28 11:27:12 -0500

    inital commit

---
+++ Operators Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿// Operators Exercise by Nick Carlson
+// Simple Program to write Results
+
+using Operators_Exercise;
+
+Console.WriteLine("Simple Math Operations: ");
+Console.WriteLine();
+
+int add = MathOps.Add(7, 7);
+int sub = MathOps.Subtract(7, 7);
+int mul = MathOps.Multiply(7, 7);
+int div = MathOps.Divide(7, 7);
+int mod = MathOps.Modulus(7, 7);
+
+Console.WriteLine($"7 + 7 when added gives you the sum of {add}; when subtracted {sub}; when multiplyed {mul}; when divided {div}; and finally with a modulus {mod}!");
+Console.WriteLine();
+
+// Declaring Integers
+Console.WriteLine("Declairng Integers Exercise 1");
+Console.WriteLine("");
+
+int a = 17;
+int b = 4;
+
+int quotient = a / b;
+int remainder = a % b;
+
+if (a == 17 && b == 4)
+    Console.WriteLine($"{a} divided by {b} is {quotient} with a remainder of {remainder}!");
+else
+    Console.WriteLine("Not the right values for the operations.");
+
+
